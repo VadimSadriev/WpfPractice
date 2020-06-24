439e678e7b688161698d1b4cee531c39a25c073f
Vadim <SadrievVF@it2g.ru>
2020-06-24 14:36:25 +0500

    Add attached property for password box

---
+++ WpfPractice/WpfPractice/AttachedProperties/Base/BaseAttachedProperty.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfPractice.AttachedProperties.Base
+{
+    /// <summary>
+    /// Base attached property
+    /// </summary>
+    /// <typeparam name="TParent">Parent class to be the attached property</typeparam>
+    /// <typeparam name="TProperty">Type of this attached property</typeparam>
+    public abstract class BaseAttachedProperty<TParent, TProperty>
+        where TParent : BaseAttachedProperty<TParent, TProperty>, new()
+    {
+        /// <summary>
+        /// Fired when value changes
+        /// </summary>
+        public event Action<DependencyObject, DependencyPropertyChangedEventArgs> ValueChanged = (sender, args) => { };
+
+        /// <summary>
+        /// A singleton instance of parent class
+        /// </summary>
+        public static TParent Instance { get; private set; } = new TParent();
+
+        /// <summary>
+        /// The actual Attached property
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.RegisterAttached("Value", typeof(TProperty), typeof(BaseAttachedProperty<TParent, TProperty>), new PropertyMetadata(new PropertyChangedCallback(OnValuePropertyChanged)));
+
+        /// <summary>
+        /// Callback event when value property has changed
+        /// </summary>
+        /// <param name="d">The UI element that has changed</param>
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // Call parent function
+            Instance.OnValueChanged(d, e);
+
+            // Call event listeners
+            Instance.ValueChanged(d, e);
+        }
+
+        /// <summary>
+        /// Gets the attached property
+        /// </summary>
+        public static TProperty GetValue(DependencyObject d)
+        {
+            return (TProperty)d.GetValue(ValueProperty);
+        }
+
+        /// <summary>
+        /// Sets the attached property
+        /// </summary>
+        public static void SetValue(DependencyObject d, TProperty value)
+        {
+            d.SetValue(ValueProperty, value);
+        }
+
+        /// <summary>
+        /// Method called when any attached property of this type is changed
+        /// </summary>
+        public virtual void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfPractice/WpfPractice/AttachedProperties/PasswordBoxAttachedProperties.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WpfPractice.AttachedProperties.Base;
+
+namespace WpfPractice.AttachedProperties
+{
+    /// <summary>
+    /// The monitor password attached property for a <see cref="PasswordBox"/>
+    /// </summary>
+    public class MonitorPasswordProperty : BaseAttachedProperty<MonitorPasswordProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var passwordBox = sender as PasswordBox;
+
+            if (passwordBox == null)
+                return;
+
+            // Remove any previous events
+            passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+
+            if ((bool)e.NewValue)
+            {
+                // set default property
+                HasTextProperty.SetValue(passwordBox);
+                passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            }
+        }
+
+        /// <summary>
+        /// Fired when password value changed
+        /// </summary>
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            HasTextProperty.SetValue((PasswordBox)sender);
+        }
+    }
+
+    /// <summary>
+    /// The HasText attached property for <see cref="PasswordBox"/>
+    /// </summary>
+    public class HasTextProperty : BaseAttachedProperty<HasTextProperty, bool>
+    {
+        /// <summary>
+        /// Sets has text property based on if caller has text
+        /// </summary>
+        public static void SetValue(DependencyObject sender)
+        {
+            SetValue(sender, ((PasswordBox)sender).SecurePassword.Length > 0);
+        }
+    }
+}
